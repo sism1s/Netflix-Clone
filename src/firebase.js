6838dfd817f633ce408e3edd7d0688741634dad5
sism1s <marko.lazic880@gmail.com>
2021-03-11 21:09:24 +0000

    set up app

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyArfHB1bJx2rgbq0hKaUdjGU5VU_1F4BU0",
+  authDomain: "netflix-clone-cd5d4.firebaseapp.com",
+  projectId: "netflix-clone-cd5d4",
+  storageBucket: "netflix-clone-cd5d4.appspot.com",
+  messagingSenderId: "427566287714",
+  appId: "1:427566287714:web:ece6dc7c2e5d92ca52ba3b",
+};
