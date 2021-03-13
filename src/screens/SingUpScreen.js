ce0fa87fdf9a525dce0235e0b362587c3a6e3a60
sism1s <marko.lazic880@gmail.com>
2021-03-13 18:39:43 +0000

    created sing Up

--- src/screens/SingUpScreen.js
+++ src/screens/SingUpScreen.js
@@ -2,7 +2,21 @@ import React from "react";
 import "./SingUpScreen.css";
 
 function SingUpScreen() {
-  return <div className="singUpScreen"></div>;
+  return (
+    <div className="singUpScreen">
+      <form>
+        <h1>Sing In</h1>
+        <input placeholder="email" type="Email" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Sing In</button>
+        <h4>
+          {" "}
+          <span className="singUpScreen__gray">New to Netflix?</span>
+          <span className="singUpScreen__link">Sing Up now.</span>
+        </h4>
+      </form>
+    </div>
+  );
 }
 
 export default SingUpScreen;
